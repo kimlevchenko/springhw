1f8749567b41b314a2865c7f1c6eb19d27b0b1dc
kimlevcenko <levchenko.kim@icloud.com>
2023-07-03 21:09:38 +0500

    init commit

---
+++ src/main/java/pro/sky/skyprospringhw/SkyproSpringHwApplication.java
@@ -0,0 +1,13 @@
+package pro.sky.skyprospringhw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkyproSpringHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SkyproSpringHwApplication.class, args);
+	}
+
+}
